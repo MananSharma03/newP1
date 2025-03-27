08411fccf6e49f6887374686779d7ebe28997ec2
MananSharma03 <sharmamanan1122@gmail.com>
2025-03-28 05:09:56 +0530

    Updated files with new changes

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
-  base: '/PortFolio/',
+  base: '/newP1/',
   plugins: [
     react(),
     runtimeErrorOverlay(),
